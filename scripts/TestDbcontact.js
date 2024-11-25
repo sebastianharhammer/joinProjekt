c190efe93c7abe127f47f373a6ef8408ddb3f1f1
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2024-11-25 18:05:49 +0100

    added svg + a test db

---
+++ scripts/TestDbcontact.js
@@ -0,0 +1,22 @@
+let contacts = [
+    {
+        firstName: "Anton",
+        lastName: "Mayer",
+        email: "antom@gmail.com"
+    },
+    {
+        firstName: "Anja",
+        lastName: "Schulz",
+        email: "schulz@hotmail.com"
+    },
+    {
+        firstName: "Benedikt",
+        lastName: "Ziegler",
+        email: "benedikt@gmail.com"
+    },
+    {
+        firstName: "David",
+        lastName: "Eisenberg",
+        email: "davidberg@gmail.com"
+    } 
+];
