90533c02800684bdd4f025765c223a1a7280959e
Daniel Loeffler <loefflerdaniel@web.de>
2024-11-26 12:42:44 +0100

    added register and login

---
+++ scripts/login.js
@@ -0,0 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+const msgBox = document.getElementById('msgBox')
+if (msg) {
+msgBox.innerHTML = msg;
+}
+else{
+msgBox.classList.add('d-none')
+}
---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+let users = [
+    {'email': 'daniel@test.de', 'password': 'test 123'}
+]
+
+function addUser(){
+    let email = document.getElementById('loginMail');
+    let password = document.getElementById('loginPassword');
+    users.push({email: email.value, password: password.value});
+    window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert'
+}
+
