88b5eda75252b97aa14f7bf69ef278bf1ee74721
Ugursay Pürcek <74987316+UG1287@users.noreply.github.com>
2024-11-27 20:42:16 +0100

    $

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,3 +1,25 @@
+let contacts = [{
+    'id': 0,
+    'forename': 'Axel',
+    'surname': 'Fowler',
+    'email': 'axel.fowler@gmail.com'
+}, {
+    'id': 1,
+    'forename': 'Leonardo',
+    'surname': 'Tornado',
+    'email': 'leonardo.tornado@gmx.de'
+},{
+    'id': 2,
+    'forename': 'Frank',
+    'surname': 'Hoppla',
+    'email': 'frank.hoppla@web.de'
+}, {
+    'id': 4,
+    'forename': 'Alexandra',
+    'surname': 'Goldschmidt',
+    'email': 'alexandra.goldschmidt@yahoo.com'
+},]
+
 function renderContactContent() {
     includeHTML();
     renderContactContentHTML();
--- templates/contact-template.js
+++ templates/contact-template.js
@@ -1,15 +1,32 @@
-function renderContactContentHTML () {
-    let content = document.getElementById('contact-content');
-    content.innerHTML += /*html*/`
+function renderContactContentHTML() {
+  let content = document.getElementById("contact-content");
+  content.innerHTML += /*html*/ `
     
     <div id="contact-side-panel">
-        <div id="add-new-contact-button-container">
-            
-            <button onclick="addContact()" id="add-contact-btn">Add new contact<img id="add-contact-img" src="./img/person_add.png"></button>
-        </div>`;
-    
 
-    content.innerHTML += /*html*/`
+    <div id="add-new-contact-button-container">
+        <button onclick="addContact()" id="add-contact-btn">Add new contact<img id="add-contact-img" src="./img/person_add.png"></button>
+    </div>
+        <ul id="contact-list">
+        <li id="contact-item-1">
+            <span id="avatar-1" style="background-color: orange;">AM</span>
+            <div>
+                <p id="contact-name-1">Anton Mayer</p>
+                <a id="contact-email-1" href="mailto:antom@gmail.com">antom@gmail.com</a>
+            </div>
+        </li>
+        <li id="contact-item-2">
+            <span id="avatar-2" style="background-color: purple;">AS</span>
+            <div>
+                <p id="contact-name-2">Anja Schulz</p>
+                <a id="contact-email-2" href="mailto:schulz@hotmail.com">schulz@hotmail.com</a>
+            </div>
+        </li>
+        </ul>
+    </div>
+    `;
+
+  content.innerHTML += /*html*/ `
     </div>
     <div id="contact-big">
         <div id="contact-headline-container">
