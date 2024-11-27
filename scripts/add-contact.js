7e87d0d384c09cede893fe07e10994b58070d533
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2024-11-27 20:34:43 +0100

    added addContact

--- scripts/add-contact.js
+++ scripts/add-contact.js
@@ -1,3 +1,5 @@
-async function addContact (firstname, lastname, email) {
-
+    function addContact () {
+        let addContactTemplate = document.getElementById('add-contact-content');
+        addContactTemplate.classList.remove('d-none');
+        addContactTemplate.innerHTML = ``;
 }
