0b56b11e6b0eb0788581c624c2161a421d9b091e
Daniel Loeffler <loefflerdaniel@web.de>
2024-11-28 12:21:43 +0100

    added script to register

--- scripts/register.js
+++ scripts/register.js
@@ -7,12 +7,24 @@ function onloadFunc(){
 const BASE_URL = "https://join-c80fa-default-rtdb.europe-west1.firebasedatabase.app/"
 let signedUsersArray="[]";
 
+async function loadUsers(path){
+    let response = await fetch(BASE_URL + path + ".json");
+    let responseToJson = await response.json();
+    if (responseToJson) {
+        signedUsersArray = Object.values(responseToJson);  
+        console.log(signedUsersArray); 
+    }
+}
+
 async function postSignUpData(path){
     let userFirstName = document.getElementById('loginFirstName').value;
     let userLastName = document.getElementById('loginLastName').value;
     let userMail = document.getElementById('loginMail').value;
     let userPassword = document.getElementById('loginPassword').value;
+    let userPasswordConfirmed = document.getElementById('loginPasswordConfirm').value
     let userId = await getNextUserId(path)
+    let checkBox = document.getElementById('registerCheckbox')
+    if(userPasswordConfirmed === userPassword && checkBox){
     let userData = {
         firstName: userFirstName,
         lastName: userLastName,
@@ -30,14 +42,42 @@ async function postSignUpData(path){
     const responseToJson = await response.json();
     signedUsersArray.push(responseToJson);
     console.log(signedUsersArray);
+}
+else{
+    console.log('Bitte akzeptiere die Privacy policy')
+}
     }
 
-async function loadUsers(path){
-    let response = await fetch(BASE_URL + path + ".json");
-    let responseToJson = await response.json();
-    if (responseToJson) {
-        signedUsersArray = Object.values(responseToJson);  
-        console.log(signedUsersArray); 
+
+function comparePasswords(){
+    let paramToCompare = document.getElementById('loginPassword').value;
+    let passwordConfirmValue = document.getElementById('loginPasswordConfirm').value;
+    let passWordBorder = document.getElementById('passWordBorder');
+    let foundPassword = false;
+    if(paramToCompare === passwordConfirmValue){
+    foundPassword = true;
+    passWordBorder.classList.remove('defaultBorderInputSignUp');
+    passWordBorder.classList.add('greenBorder')
+    }else{
+    foundPassword = false;
+    passWordBorder.classList.remove('defaultBorderInputSignUp');
+    passWordBorder.classList.add('redBorder')
+    }
+showResultsMessage(foundPassword)
+}
+
+function showResultsMessage(foundPassword){
+    let alertDiv = document.getElementById('alert-password');
+    alertDiv.innerHTML = '';
+    if(foundPassword){
+        alertDiv.innerHTML += /*html*/`
+            <p>Passswort stimmt</p>
+        `
+    }
+    else{
+        alertDiv.innerHTML += /*html*/`
+            <p>Das Passwort stimmt nicht überein</p>
+        `
     }
 }
 
