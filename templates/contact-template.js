8552cfdbdd6ff5293cbc4d330997b112d21936fd
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2024-11-29 14:57:07 +0100

    added basic structure

--- templates/contact-template.js
+++ templates/contact-template.js
@@ -50,18 +50,30 @@ function showContactDetailsHTML(id, firstname, lastname, email, phone) {
     console.log(id, firstname, lastname, email, phone);
     let contactDetailContent = document.getElementById('detailed-contact-info');
     contactDetailContent.innerHTML = /*html*/`
-    <div id="contact-detail-short-name>
-        <svg class="customCircle" width="50" height="50">
+    <div id="contact-detail-short-name">
+        <svg class="customCircle" width="200" height="200">
             <circle id="user-circle" class="circleBorder" cx="50%" cy="50%" r="24" stroke="rgb(42,54,71)" stroke-width="2" fill="white"></circle>
             <text class="textInCircle" x="50%" y="50%" text-anchor="middle" alignment-baseline="central">${getFirstLetter(firstname)}${getFirstLetter(lastname)}</text>
         </svg>
     </div>
     <div id="contact-detail-contact-interactions">
-        <span id="contact-detail-email">${email}</span>
+        <span id="contact-detail-name">${firstname} ${lastname}</span>
         <div id="contact-detail-btn-container">
+            <div class="contact-detail-btn-wrapper">
+                <img id="edit-pencil-icon" src="../img/edit-pencil.png">
             <button class="contact-detail-btn" id="contact-detail-delete-btn" onclick="deleteContact('${id}')">Delete</button>
+            </div>
+            <div class="contact-detail-btn-wrapper">
+                <img id="delete-icon" src="../img/delete.png">
             <button class="contact-detail-btn" id="contact-detail-edit-btn"onclick="editConctact('${id}')">Edit</button>
+            </div>
         </div>
+        <div id="contact-detail-more-info-container">
+            <h5 id="ci-headline">Contact Information</h5>
+            <span id="email-description">Email</span>
+            <span id="contact-detail-email">${email}</span>
+            <span id="phone-description">Phone</span>
+            <span id="contact-detail-phone">${phone}</span>
     </div>
     `;
 }
