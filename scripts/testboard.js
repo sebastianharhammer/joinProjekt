9676f1ae5b5fc5875cd1a206c1499818f42f043c
Daniel Loeffler <loefflerdaniel@web.de>
2024-12-03 13:33:25 +0100

    added boardscript

--- scripts/testboard.js
+++ scripts/testboard.js
@@ -1,17 +1,17 @@
-function init(){
+function init() {
     includeHTML();
     fetchTasks("/tasks");
     loadBoardNavigator();
 }
 
-const BASE_URL= "https://join-c80fa-default-rtdb.europe-west1.firebasedatabase.app/";
-let taskArray=[];
+const BASE_URL = "https://join-c80fa-default-rtdb.europe-west1.firebasedatabase.app/";
+let taskArray = [];
 
-async function fetchTasks(path=""){
+async function fetchTasks(path = "") {
     let response = await fetch(BASE_URL + path + ".json");
     let responseToJson = await response.json();
     console.log(responseToJson)
-    if(responseToJson){
+    if (responseToJson) {
         taskArray = Object.values(responseToJson);
     }
     console.log(taskArray)
@@ -20,14 +20,14 @@ async function fetchTasks(path=""){
 
 
 
-function loadBoardNavigator(){
+function loadBoardNavigator() {
     let content = document.getElementById('wholeBoard');
     content.innerHTML = '';
     content.innerHTML += getBoardNavigatorHTML();
     loadTitleOfBoardColumns(content);
 }
 
-function getBoardNavigatorHTML(){
+function getBoardNavigatorHTML() {
     return /*html*/`
 <section class="boardNavigator">
     <div class="searchAndAddTasks">
@@ -47,12 +47,12 @@ function getBoardNavigatorHTML(){
     `
 }
 
-function loadTitleOfBoardColumns(content){
-content.innerHTML+= showTitleOfBoardColumns();
-getColumns(content);
+function loadTitleOfBoardColumns(content) {
+    content.innerHTML += showTitleOfBoardColumns();
+    getColumns(content);
 }
 
-function showTitleOfBoardColumns(){
+function showTitleOfBoardColumns() {
     return /*html*/`
         <section id="titleOfBoardColumns" class="titleOfBoardColumns">
 <div class="columntitleToDo">
@@ -75,11 +75,11 @@ function showTitleOfBoardColumns(){
     `
 }
 
-function getColumns(content){
-    content.innerHTML+= getColumnsHTML();
+function getColumns(content) {
+    content.innerHTML += getColumnsHTML();
 }
 
-function getColumnsHTML(){
+function getColumnsHTML() {
     return /*html*/`
     <section class="tasksContent">
     <div class="dragarea-todo" id="todo" ondrop="moveTo('todo')" ondragleave="removeHighlight('todo')" ondragover="allowDrop(event); highlight('todo')"></div>
@@ -91,40 +91,71 @@ function getColumnsHTML(){
 }
 
 
-function updateTaskHTML(){
-let todoColumn = document.getElementById("todo");
-let inProgressColumn = document.getElementById("inProgress");
-let feedbackColumn = document.getElementById("feedback");
-let doneColumn = document.getElementById("done");
-todoColumn.innerHTML = '';
-inProgressColumn.innerHTML = '';
-feedbackColumn.innerHTML = '';
-doneColumn.innerHTML = '';
-let todos = taskArray.filter(task => task.status === "todo");
-let inProgress = taskArray.filter(task => task.status === "inProgress");
-let feedback = taskArray.filter(task => task.status === "feedback");
-let done = taskArray.filter(task => task.status === "done");
-for (const task of todos) {
-    todoColumn.innerHTML += createTaskHTML(task);
-}
-for (const task of inProgress) {
-    inProgressColumn.innerHTML += createTaskHTML(task);
-}
-for (const task of feedback) {
-    feedbackColumn.innerHTML += createTaskHTML(task);
-}
-for (const task of done) {
-    doneColumn.innerHTML += createTaskHTML(task);
-}
+function updateTaskHTML() {
+    let todoColumn = document.getElementById("todo");
+    let inProgressColumn = document.getElementById("inProgress");
+    let feedbackColumn = document.getElementById("feedback");
+    let doneColumn = document.getElementById("done");
+    todoColumn.innerHTML = '';
+    inProgressColumn.innerHTML = '';
+    feedbackColumn.innerHTML = '';
+    doneColumn.innerHTML = '';
+    let todos = taskArray.filter(task => task.status === "todo");
+    let inProgress = taskArray.filter(task => task.status === "inProgress");
+    let feedback = taskArray.filter(task => task.status === "feedback");
+    let done = taskArray.filter(task => task.status === "done");
+    for (const task of todos) {
+        todoColumn.innerHTML += createTaskHTML(task);
+        createOwnerCircles(task);
+        findClassOfTaskCat(task);
+    }
+    for (const task of inProgress) {
+        inProgressColumn.innerHTML += createTaskHTML(task);
+        createOwnerCircles(task);
+        findClassOfTaskCat(task);
+    }
+    for (const task of feedback) {
+        feedbackColumn.innerHTML += createTaskHTML(task);
+        createOwnerCircles(task);
+        findClassOfTaskCat(task);
+    }
+    for (const task of done) {
+        doneColumn.innerHTML += createTaskHTML(task);
+        createOwnerCircles(task);
+        findClassOfTaskCat(task);
+    }
 }
 
-function getOwners(task){
-let owners = [];
-for(let i = 0; i < task.owner.length; i++){
-    let owner = task.owner[i];
-    owners.push(`${owner.firstName} ${owner.lastName}`);
+function createOwnerCircles(task) {
+    let userNameCircles = document.getElementById(`userNameCircles-${task.id}`);
+    userNameCircles.innerHTML = '';
+    for (let owner of task.owner) {
+        let initialsOfOwner = `${owner.firstName.charAt(0)}${owner.lastName.charAt(0)}`.toUpperCase();
+        userNameCircles.innerHTML += /*html*/`
+            <svg width="34" height="34">
+                <circle cx="50%" cy="50%" r="16" stroke="white" stroke-width="1" fill="rgb(255,122,0)" />
+                <text class="fontInNameCircle" x="50%" y="50%" text-anchor="middle" alignment-baseline="central">${initialsOfOwner}</text>
+            </svg>
+        `
+    }
 }
-return owners.join(", ")
+
+function findClassOfTaskCat(task) {
+        const taskButton = document.getElementById(`taskButton-${task.id}`);
+        if (task.taskCategory === "Technical Task") {
+            taskButton.classList.add("task-category-technicalTask");
+        } else if (task.taskCategory === "User Story") {
+            taskButton.classList.add("task-category-userExperience");
+        }
+}
+
+function getOwners(task) {
+    let owners = [];
+    for (let i = 0; i < task.owner.length; i++) {
+        let owner = task.owner[i];
+        owners.push(`${owner.firstName} ${owner.lastName}`);
+    }
+    return owners.join(", ")
 }
 
 function getSubTasks(task) {
@@ -137,7 +168,7 @@ function getSubTasks(task) {
 }
 
 
-function createTaskHTML(task){
+function createTaskHTML(task) {
     const owners = getOwners(task);
     const subtasks = getSubTasks(task)
     return /*html*/`
@@ -148,21 +179,8 @@ function createTaskHTML(task){
         <p class= "open-sans-bold">${task.title}</p>
         <p class="inter-font">${task.description}</p>
         <p class="inter-font">${subtasks}</p>
-        <p class="inter-font">${owners}</p>
         <section class="namesAndPrio">
-        <div class="userNameCircles">
-            <svg width="34" height="34">
-                <circle cx="50%" cy="50%" r="16" stroke="white" stroke-width="1" fill="rgb(255,122,0)" />
-                <text class="fontInNameCircle" x="50%" y="50%" text-anchor="middle" alignment-baseline="central">DL</text>
-            </svg>
-            <svg class="addMarginToOverlay" width="34" height="34">
-                <circle cx="50%" cy="50%" r="16" stroke="white" stroke-width="1" fill="rgb(31,215,193)" />
-                <text class="fontInNameCircle" x="50%" y="50%" text-anchor="middle" alignment-baseline="central">EN</text>
-            </svg>
-            <svg class="addMarginToOverlay" width="34" height="34">
-                <circle cx="50%" cy="50%" r="16" stroke="white" stroke-width="1" fill="rgb(110,82,255)" />
-                <text class="fontInNameCircle" x="50%" y="50%" text-anchor="middle" alignment-baseline="central">AR</text>
-            </svg>
+        <div class="userNameCircles" id="userNameCircles-${task.id}">
         </div>
         </section>
         </div>
