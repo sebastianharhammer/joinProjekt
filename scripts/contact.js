493cd94ae0bb71b4520c7b7b3ab9efceb1dbea6a
Ugursay Pürcek <74987316+UG1287@users.noreply.github.com>
2024-12-08 17:11:29 +0100

    edit-contact update

--- scripts/contact.js
+++ scripts/contact.js
@@ -113,19 +113,17 @@ function toggleContactDetail(contactId) {
       <div id="contact-information">Contact Information</div>
 
           <div id="contact-detail-bottom">
-          <div id="contact-detail-email">
-          <div id="contact-detail-bottom-font">Email:</div> 
-          <div> <a href="mailto:${selectedContact.email}">${
+            <div id="contact-detail-email">
+              <div id="contact-detail-bottom-font">Email:</div> 
+              <div> <a href="mailto:${selectedContact.email}">${
       selectedContact.email
     }</a></div>
-          </div>
+            </div>
           <div id="contact-detail-phone">
-          <div id="contact-detail-bottom-font">Phone:</div> <div> ${
-            selectedContact.phone
-          }</div>
+            <div id="contact-detail-bottom-font">Phone:</div> 
+            <div> ${selectedContact.phone}</div>
           </div>
           </div>
-      </div>
     `;
   }
 }
