2540fef2065017e7ceb221d78b850a0834682f16
Daniel Loeffler <loefflerdaniel@web.de>
2024-12-08 23:06:30 +0100

    greeting name after login works now

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,10 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const urlParams2 = new URLSearchParams(window.location.search);
-    const welcomeMsg = urlParams2.get('welcomeMsg');
-    if (welcomeMsg) {
-        const welcomeDiv = document.getElementById('welcomeMsg');
-        if (welcomeDiv) {
-            welcomeDiv.innerHTML = welcomeMsg;
-        }
+async function initSummary() {
+    await includeHTML();
+    const urlParams = new URLSearchParams(window.location.search);
+    const firstName = urlParams.get('firstName');
+    const lastName = urlParams.get('lastName');
+    if (firstName && lastName) {
+        showUserGreeting(firstName, lastName);
+    } else {
+        console.error("no registered user found");
     }
-});
+}
+
+
+async function fetchUser(userId) {
+    try {
+        const userPath = `/signed_users/user${userId}.json`;
+        const response = await fetch(BASE_URL + userPath);
+        const userData = await response.json();
+        if (userData) {
+            console.log("Benutzerdaten erfolgreich abgerufen:", userData);
+        } else {
+            console.error("Benutzer nicht gefunden in Firebase");
+        }
+    } catch (error) {
+        console.error("Fehler beim Abrufen der Benutzerdaten aus Firebase:", error);
+    }
+    
+}
+
+function showUserGreeting(firstName, lastName) {
+    const greetingName = document.getElementById('nameOfUser');
+    if (greetingName) {
+        greetingName.innerHTML = `<p>${firstName} ${lastName}</p>`;
+    } else {
+        console.error("Das Element #nameOfUser wurde nicht gefunden.");
+    }
+}
