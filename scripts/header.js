4a4cbf69058b8144187c3432351c669085775862
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2024-12-09 11:54:48 +0100

    displays Name

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,29 @@
+let firstName = "";
+let lastName = "";
+
+window.addEventListener("load", function() {
+    HeaderGetUser();
+});
+
 function showUserOptions() {
     let userOption = document.getElementById('user-overlay');
     userOption.classList.toggle('d-none')
 }
 
-async function getUser(user) {
-    console.log("headerUser ist:" + user);
+
+function HeaderGetUser() {
+    let object = localStorage.getItem("currentUser");
+    let objectToJson = JSON.parse(object);
+    firstName = objectToJson.firstName;
+    lastName = objectToJson.lastName;
+    console.log("Header Local Storage Name: " + firstName + " - " + lastName);
+    headerDisplayName(firstName, lastName);
+}
+
+
+function headerDisplayName(firstName, lastName) {
+        firstNameShort = firstName.trim().charAt(0).toUpperCase();
+        lastNameShort = lastName.trim().charAt(0).toUpperCase();
+        let userId = document.getElementById('header-user-name');
+        userId.innerHTML = `${firstNameShort}${lastNameShort}`;
 }
