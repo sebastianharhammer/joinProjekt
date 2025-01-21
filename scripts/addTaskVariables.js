6f81ade2e67164f6148dca0cfcbd9695324fa0df
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-01-21 10:24:54 +0100

    delted jsdoc

--- scripts/addTaskVariables.js
+++ scripts/addTaskVariables.js
@@ -1,63 +1,25 @@
-/**
- * Indicates whether the categories container was clicked.
- * @type {boolean}
- */
 let categoriesContainerClick = false;
 
-/**
- * Array for storing contacts.
- * @type {Array}
- */
 let contacts = [];
 
-/**
- * The currently selected priority.
- * @type {string}
- */
 let selectedPriority = "medium";
 
-/**
- * Array for storing subtasks.
- * @type {Array}
- */
 let subtasksArr = [];
 
-/**
- * Array for storing assigned users.
- * @type {Array}
- */
 let assignedUserArr = [];
 
-/**
- * Local storage of tasks.
- * @type {Array}
- */
 let localTasks = [];
 
-/**
- * Counter for subtask IDs.
- * @type {number}
- */
 let subtaskIdCounter = 0;
 
-/**
- * Arrays for editing subtasks.
- * @type {Array}
- */
 let subtasksEdit = [];
+
 let subtasksEdit_done = [];
+
 let subtasksArr_done = [];
 
-/**
- * Current category object.
- * @type {string}
- */
 let categoryObject = "";
 
-/**
- * List of addable task categories with background colors.
- * @type {Array<Object>}
- */
 let addTaskcategories = [
   { category: "User Story", "bg-color": "#0038FF" },
   {
@@ -66,8 +28,4 @@ let addTaskcategories = [
   },
 ];
 
-/**
- * Array for storing assigned users.
- * @type {Array}
- */
 let assignedUser = [];
